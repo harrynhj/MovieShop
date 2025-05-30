27d316dc3e61cc896b53607783d4737eeb78c9cd
Haoji Ni <hni7@illinois.edu>
2025-05-29 19:47:27 -0500

    initialize files

---
+++ MovieShop/Controllers/AdminController.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieShop.Controllers;
+
+public class AdminController : Controller
+{
+    
+}
---
+++ MovieShop/Controllers/CastController.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieShop.Controllers;
+
+public class CastController : Controller
+{
+    
+}
